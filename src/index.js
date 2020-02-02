8ea30491067223566caa5369c400fc0e72ae0d70
Salma Kolkari <34543416+skolkari@users.noreply.github.com>
2020-02-02 21:51:12 +0530

    site files added

--- src/index.js
+++ src/index.js
@@ -14,10 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        
       </div>
     );
   }
